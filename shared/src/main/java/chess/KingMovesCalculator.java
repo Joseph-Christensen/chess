ed77d9aac23b43af2663c7d9f2332ba3b86ca7dc
Joseph-Christensen <josephchristensen06@gmail.com>
2025-10-22 13:40:13 -0600

    Generic function implemented for King and Knight

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -3,133 +3,19 @@ package chess;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class KingMovesCalculator {
+public class KingMovesCalculator extends PieceMovesCalculator {
 
-    private final ChessBoard board;
-    private final ChessPosition position;
-    private final ChessPiece piece;
-
-    KingMovesCalculator(ChessBoard board, ChessPosition position, ChessPiece piece) {
-        this.board = board;
-        this.position = position;
-        this.piece = piece;
+    public KingMovesCalculator(ChessBoard board, ChessPosition position, ChessPiece piece) {
+        super(board, position, piece);
     }
 
-    public ChessMove processMove(ChessPosition currPos, ChessGame.TeamColor team) {
-        if (board.getPiece(currPos) == null) {
-            // no piece
-            return new ChessMove(position, currPos, null);
-        }
-        else {
-            // piece detected
-            if (board.getPiece(currPos).getTeamColor() != team) {
-                // different team
-                return new ChessMove(position, currPos, null);
-            }
-        }
-        return null;
-    }
-
+    @Override
     public Collection<ChessMove> returnMoves() {
         var moves = new HashSet<ChessMove>();
-        ChessGame.TeamColor team = piece.getTeamColor();
-
-        for (int i = 0; i < 8; i++) {
-            // goes through 8 times for each direction
-            int row = position.getRow();
-            int col = position.getColumn();
-
-            if (i == 0) {
-                // left
-                col--;
-                if (col > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 1) {
-                // up-left
-                col--;
-                row++;
-                if (col > 0 && row < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 2) {
-                // up
-                row++;
-                if (row < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 3) {
-                // up-right
-                col++;
-                row++;
-                if (col < 9 && row < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 4) {
-                // right
-                col++;
-                if (col < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 5) {
-                // down-right
-                col++;
-                row--;
-                if (col < 9 && row > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 6) {
-                // down
-                row--;
-                if (row > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else {
-                // down-left
-                col--;
-                row--;
-                if (col > 0 && row > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
+        int[][] directions = {{-1,1},{1,1},{1,-1},{-1,-1},{1,0},{0,1},{-1,0},{0,-1}};
+        for (int[] direction : directions) {
+            if (positionMoveCalc(direction[0], direction[1], board, position, piece.getTeamColor()) != null) {
+                moves.add(positionMoveCalc(direction[0], direction[1], board, position, piece.getTeamColor()));
             }
         }
         return moves;
--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -3,145 +3,19 @@ package chess;
 import java.util.Collection;
 import java.util.HashSet;
 
-public class KnightMovesCalculator {
+public class KnightMovesCalculator extends PieceMovesCalculator {
 
-    private final ChessBoard board;
-    private final ChessPosition position;
-    private final ChessPiece piece;
-
-    KnightMovesCalculator(ChessBoard board, ChessPosition position, ChessPiece piece) {
-        this.board = board;
-        this.position = position;
-        this.piece = piece;
+    public KnightMovesCalculator(ChessBoard board, ChessPosition position, ChessPiece piece) {
+        super(board, position, piece);
     }
 
-    public ChessMove processMove(ChessPosition currPos, ChessGame.TeamColor team) {
-        if (board.getPiece(currPos) == null) {
-            // no piece
-            return new ChessMove(position, currPos, null);
-        }
-        else {
-            // piece detected
-            if (board.getPiece(currPos).getTeamColor() != team) {
-                // different team
-                return new ChessMove(position, currPos, null);
-            }
-        }
-        return null;
-    }
-
+    @Override
     public Collection<ChessMove> returnMoves() {
         var moves = new HashSet<ChessMove>();
-        ChessGame.TeamColor team = piece.getTeamColor();
-
-        for (int i = 0; i < 8; i++) {
-            // goes through 8 times for each direction
-            int row = position.getRow();
-            int col = position.getColumn();
-
-            if (i == 0) {
-                // left-up
-                col--;
-                col--;
-                row++;
-                if (col > 0 && row < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 1) {
-                // up-left
-                col--;
-                row++;
-                row++;
-                if (col > 0 && row < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 2) {
-                // up-right
-                row++;
-                row++;
-                col++;
-                if (row < 9 && col < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 3) {
-                // right-up
-                col++;
-                col++;
-                row++;
-                if (col < 9 && row < 9) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 4) {
-                // right-down
-                col++;
-                col++;
-                row--;
-                if (col < 9 && row > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 5) {
-                // down-right
-                row--;
-                row--;
-                col++;
-                if (col < 9 && row > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else if (i == 6) {
-                // down-left
-                row--;
-                row--;
-                col--;
-                if (row > 0 && col > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
-            }
-            else {
-                // left-down
-                col--;
-                col--;
-                row--;
-                if (col > 0 && row > 0) {
-                    // continue
-                    ChessPosition currPos = new ChessPosition(row, col);
-                    if (processMove(currPos, team) != null) {
-                        moves.add(processMove(currPos, team));
-                    }
-                }
+        int[][] directions = {{-2,1},{-1,2},{1,2},{2,1},{2,-1},{1,-2},{-1,-2},{-2,-1}};
+        for (int[] direction : directions) {
+            if (positionMoveCalc(direction[0], direction[1], board, position, piece.getTeamColor()) != null) {
+                moves.add(positionMoveCalc(direction[0], direction[1], board, position, piece.getTeamColor()));
             }
         }
         return moves;
