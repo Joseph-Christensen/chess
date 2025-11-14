4869e098ecb54d7c961465dc7c91480d16139fb2
Joseph-Christensen <josephchristensen06@gmail.com>
2025-11-13 19:46:04 -0700

    Chessboard Display fully debugged

--- client/src/main/java/ui/ChessboardDisplay.java
+++ client/src/main/java/ui/ChessboardDisplay.java
@@ -1,14 +1,12 @@
 package ui;
 
 import java.io.PrintStream;
-import java.nio.charset.StandardCharsets;
 import static ui.EscapeSequences.*;
 
 public class ChessboardDisplay {
 
     private static final int SIZE = 8;
     private static final int SQUARE_WIDTH = 3;
-    private static final int SQUARE_HEIGHT = 3;
 
     private static final String EMPTY = " ".repeat(SQUARE_WIDTH);
 
@@ -40,7 +38,7 @@ public class ChessboardDisplay {
         out.print(SET_BG_COLOR_LIGHT_GREY + SET_TEXT_COLOR_BLACK);
         out.print(EMPTY);
         for (char c = 'a'; c <= 'h'; c++) {
-            out.print("  " + c + " ");
+            out.print(" " + c + " ");
         }
         out.print(EMPTY);
         out.println(RESET);
@@ -50,7 +48,7 @@ public class ChessboardDisplay {
         out.print(SET_BG_COLOR_LIGHT_GREY + SET_TEXT_COLOR_BLACK);
         out.print(EMPTY);
         for (char c = 'h'; c >= 'a'; c--) {
-            out.print("  " + c + " ");
+            out.print(" " + c + " ");
         }
         out.print(EMPTY);
         out.println(RESET);
@@ -170,7 +168,7 @@ public class ChessboardDisplay {
                 out.print(SET_BG_COLOR_BLACK);
                 whiteBackground = true;
             }
-            out.print("  " + order[i] + " ");
+            out.print(" " + order[i] + " ");
         }
         out.print(RESET);
     }
