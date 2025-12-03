8b81f4d40f57a487a4335a157692ab6ad4e4af79
Joseph-Christensen <josephchristensen06@gmail.com>
2025-12-03 15:57:50 -0700

    MessageHandler now handles all kinds of Messages

--- shared/src/main/java/server/WebSocketFacade.java
+++ shared/src/main/java/server/WebSocketFacade.java
@@ -13,11 +13,14 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import jakarta.websocket.ClientEndpoint;
+
 @ClientEndpoint
-public class WebSocketFacade extends Endpoint {
+public class WebSocketFacade {
 
     Session session;
     NotificationHandler notificationHandler;
+    Gson gson = new Gson();
 
     public WebSocketFacade(String url, NotificationHandler notificationHandler) throws ResponseException {
         try {
@@ -28,17 +31,33 @@ public class WebSocketFacade extends Endpoint {
             WebSocketContainer container = ContainerProvider.getWebSocketContainer();
             this.session = container.connectToServer(this, socketURI);
 
-            //set message handler
-            this.session.addMessageHandler(new MessageHandler.Whole<String>() {
-                @OnMessage
-                public void onMessage(String message) {
-                    ServerMessage notification = new Gson().fromJson(message, ServerMessage.class);
-                    notificationHandler.notify(notification);
-                }
-            });
+
         } catch (DeploymentException | IOException | URISyntaxException ex) {
             throw new ResponseException(ResponseException.Code.ServerError, ex.getMessage());
         }
-    }
 
+        //set message handler
+        this.session.addMessageHandler(new MessageHandler.Whole<String>() {
+            @OnMessage
+            public void onMessage(String message) {
+                ServerMessage base = gson.fromJson(message, ServerMessage.class);
+
+                switch (base.getServerMessageType()) {
+                    case LOAD_GAME -> {
+                        LoadGameMessage loadGame = gson.fromJson(message, LoadGameMessage.class);
+                        notificationHandler.notify(loadGame);
+                    }
+                    case ERROR -> {
+                        ErrorMessage error = gson.fromJson(message, ErrorMessage.class);
+                        notificationHandler.notify(error);
+                    }
+                    case NOTIFICATION -> {
+                        notificationHandler.notify(base);
+                    }
+                }
+            }
+        });
+
+
+    }
 }
